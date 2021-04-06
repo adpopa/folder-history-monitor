cea6cb6ee1effc9b2260e60ccc475255206b39c1
alex <alexdanielpopa@outlook.com>
2021-04-06 23:36:52 +0300

    first commit

---
+++ src/main/java/com/sisw/alexpopa/folderhistorymonitor/FolderHistoryMonitorApplication.java
@@ -0,0 +1,14 @@
+package com.sisw.alexpopa.folderhistorymonitor;
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FolderHistoryMonitorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FolderHistoryMonitorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sisw/alexpopa/folderhistorymonitor/FolderHistoryMonitorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sisw.alexpopa.folderhistorymonitor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FolderHistoryMonitorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
