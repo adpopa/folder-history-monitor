ee4ef81cbfc699fd1e077ed26bdc9548ece04bbc
alex <alexdanielpopa@outlook.com>
2021-04-07 19:07:34 +0300

    commit

---
+++ src/test/java/com/sisw/alexpopa/folderhistorymonitor/service/DirectoryMonitorServiceUnitTest.java
@@ -0,0 +1,16 @@
+package com.sisw.alexpopa.folderhistorymonitor.service;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.springframework.boot.test.context.TestConfiguration;
+
+/**
+ * @author Alex Daniel Popa
+ */
+public class DirectoryMonitorServiceUnitTest {
+
+    @BeforeAll
+    static void setup() {
+
+    }
+
+}
