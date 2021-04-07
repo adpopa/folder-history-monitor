d9ca73e290efaf5fd4ed4a67989631025c9b8b16
alex <alexdanielpopa@outlook.com>
2021-04-07 18:24:57 +0300

    unit test suite for DirectoryMonitorServiceProperties class

---
+++ src/test/java/com/sisw/alexpopa/folderhistorymonitor/properties/DirectoryMonitorServicePropertiesUnitTest.java
@@ -0,0 +1,34 @@
+package com.sisw.alexpopa.folderhistorymonitor.properties;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * @author Alex Daniel Popa
+ */
+public class DirectoryMonitorServicePropertiesUnitTest {
+
+    @Test
+    public void testAccessorsMutators() {
+        DirectoryMonitorServiceProperties dmsp = new DirectoryMonitorServiceProperties();
+
+        dmsp.setDirectoryPath("pathToDir");
+
+        assertEquals("pathToDir", dmsp.getDirectoryPath());
+
+    }
+
+    @Test
+    public void whenNullPointerException_thenAssertionSucceeds() {
+        DirectoryMonitorServiceProperties dmsp = new DirectoryMonitorServiceProperties();
+
+        Exception exception = assertThrows(NullPointerException.class, () -> dmsp.setDirectoryPath(null));
+
+        String expectedMessage = "is marked non-null but is null";
+        String actualMessage = exception.getMessage();
+
+        assertTrue(actualMessage.contains(expectedMessage));
+    }
+
+}
