d56dcefe8c4152705eb11e28aed833612da7b543
alex <alexdanielpopa@outlook.com>
2021-04-13 12:05:25 +0300

    final commit

--- src/main/java/com/sisw/alexpopa/folderhistorymonitor/FolderHistoryMonitorApplication.java
+++ src/main/java/com/sisw/alexpopa/folderhistorymonitor/FolderHistoryMonitorApplication.java
@@ -1,11 +1,8 @@
 package com.sisw.alexpopa.folderhistorymonitor;
 
-//import com.sisw.alexpopa.folderhistorymonitor.config.MyAppConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-
 
 @SpringBootApplication
 @EnableConfigurationProperties
